3fdff1d9d066bbaeed85c9d5d074b24fb4935688
Daniel_Setlik <daniel.setlik@capgemini.com>
2022-11-23 06:38:32 +0100

    Admin Layout

---
+++ src/app/layouts/fullpage-admin/fullpage-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fullpage-admin',
+  templateUrl: './fullpage-admin.component.html',
+  styleUrls: ['./fullpage-admin.component.scss']
+})
+export class FullpageAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
